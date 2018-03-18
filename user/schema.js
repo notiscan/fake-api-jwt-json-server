318354c245b93371109c6b5c24e8a3388f8c3f28
Emmanuel Addo Narh <eanarh@gmail.com>
2018-03-17 20:57:51 -0700

    chore: refactored user schema

--- user/schema.js
+++ user/schema.js
@@ -1,22 +1,26 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    first: { type: String, trim: true, required: true },
-    last: { type: String, trim: true, required: true }
-  },
+  accounts: [],
   email: { type: String, trim: true, required: true, unique: true },
+  firstname: { type: String, trim: true, required: true },
+  lastname: { type: String, trim: true, required: true },
   password: { type: String, trim: true, required: true },
-  accounts: []
+  passwords: [{ type: String, trim: true, default: '' }]
 },
 {
   timestamps: true
 });
 
-userSchema.pre('save', (next) => {
-  const currentDate = new Date();
-  this.updated_at = currentDate;
-  if (!this.created_at) this.created_at = currentDate;
+userSchema.pre('save', function (next) {
+  if (this.passwords.includes(this.password)) {
+    const err = new Error('something went wrong');
+    next(err); return;
+  }
+
+  if (!this.passwords.includes(this.password)) {
+    this.passwords.push(this.password);
+  }
   next();
 });
 
